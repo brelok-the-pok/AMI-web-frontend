958544fe8283af60eb72b83f448896756b17a410
pok <tolochek10@mail.ru>
2023-12-27 21:40:07 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import {PlotSettingsModal} from "./PlotSettingsModal"
+import {ImageModal} from "./ImageModal"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [modalActive, setModalActive] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const [images, setImages] = useState([
+        // 'http://127.0.0.1:8000/data/plots/lb-Pclass.svg',
+        // 'http://127.0.0.1:8000/data/plots/lb-Pclass.svg',
+        // 'http://127.0.0.1:8000/data/plots/lb-Pclass.svg',
+        // 'http://127.0.0.1:8000/data/plots/lb-Pclass.svg',
+    ]);
+
+
+    const handleChange = async (event) => {
+        setSelectedFile(event.target.files[0])
+        if (event.target.files[0]) {
+
+            const formData = new FormData()
+            formData.append('file', event.target.files[0])
+            const res = await fetch(
+                "http://127.0.0.1:8000/rest/loader/load-mono/",
+                {method: 'POST', body: formData}
+            )
+            const data = await res.json()
+
+            alert(data)
+
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="main_block">
+                <div className="load_div">
+                    <input type="file" onChange={handleChange} />
+                </div>
+
+                <br/>
+                <div>
+                    <ImageModal images={images}/>
+                </div>
+                <br/>
+                <div className={"popup_button"}>
+                    <button onClick={() => {setModalActive(true)}} className="popup_button">Построить графики</button>
+                </div>
+
+
+            </div>
+
+            <PlotSettingsModal active={modalActive} setActive={setModalActive} setImages={setImages}/>
+
+        </div>
+    )
 }
+
 
 export default App;
